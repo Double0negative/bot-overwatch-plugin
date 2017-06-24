422bb27f6c89d417eab679338a3db6c8cd4c8fb7
Double0negative <drew@superleague.com>
2017-06-24 15:45:26 -0700

    Scrap it all! Starting from scratch

--- src/main/java/org/mcsg/bot/overwatch/LinkAccountCommand.groovy
+++ src/main/java/org/mcsg/bot/overwatch/LinkAccountCommand.groovy
@@ -18,40 +18,7 @@ public class LinkAccountCommand implements BotCommand{
 	public void execute(String cmd, BotServer server, BotChannel chat, BotUser user, String[] args, String input)
 	throws Exception {
 
-		try{
-			if(args.length == 1) {
-
-				if(args[0] == "delete") {
-					plugin.manager.unLinkAccount(user.getId())
-					chat.sendMessage("Unlinked OVerwatch account")
-				} else {
-					plugin.getManager().linkAccount(server.getId(), user, args[0].replace("#", "-"), "")
-					chat.sendMessage("Linked Overwatch account")
-				}
-			} else if (args.length > 1){
-				if(args[0] == "verify" && server.getBot().getPermissionManager().hasPermission(server, user,  "ow.verify")){
-					plugin.manager.verify(server, args[1])
-					chat.sendMessage("Verified account")
-				}
-				if(args[0] == "delete") {
-					plugin.manager.unLinkAccount(server.getId(), args[0], args[1])
-					chat.sendMessage("Unlinked OVerwatch account")
-				}
-				if(args[0] == "other" && server.getBot().getPermissionManager().hasPermission(server,user,  "ow.admin.link")) {
-					plugin.getManager().linkAccount(server.getId(), args[1], args[2])
-					chat.sendMessage("Linked overwatch account")
-				} else {
-					plugin.getManager().linkAccount(server.getId(), user, args[0].replace("#", "-"), args[1].toLowerCase())
-					chat.sendMessage("Linked Overwatch account")
-				}
-			}
-		}catch (ProfileNotFound e) {
-			chat.sendMessage("Profile not found");
-			e.printStackTrace()
-		}
-
 	}
-
 	@Override
 	public String getPermission() {
 		return "ow.user.link";
--- src/main/java/org/mcsg/bot/overwatch/StatsCommand.groovy
+++ src/main/java/org/mcsg/bot/overwatch/StatsCommand.groovy
@@ -23,79 +23,8 @@ public class StatsCommand implements BotCommand{
 	public void execute(String cmd, BotServer server, BotChannel chat, BotUser user, String[] args, String input)
 	throws Exception {
 
-		def player;
-		def region;
-		if(args.length == 0) {
-			player = user.getUsername();
-		}
-		else if(args.length > 0) {
-			player = args[0].replace("#", "-");
-		}
-		def mode = 'competitive'
+		
 
-		if(args.length > 1) {
-			if(args[1].toLowerCase() == "quickplay")
-				mode = "quickplay"
-			else
-				region = args[1].toLowerCase()
-		}
-
-		try{
-			def root = plugin.getManager().getStats(server.getId(), player, region);
-			def stats = root.stats[mode]
-			def overall = stats.overall_stats
-			def avg = stats.average_stats
-			def game = stats.game_stats
-			def heroes_stats = root.heroes.stats[mode]
-
-			def heroes_time = root.heroes.playtime[mode]
-			heroes_time = heroes_time.sort { a, b ->
-				-(a.value <=> b.value)
-			}
-
-			def heroes = getHeroes(game.time_played, heroes_time, heroes_stats)
-
-			def str = """
-				```
-${mode == 'competitive' ? 'Competitive' : 'Quick Play'} stats for ${root.user.split("-")[0]}
-  Level: ${overall.level + 100 * overall.prestige}
-
-${mode == 'competitive' ? 'Season 4' : 'Quick Play'}
-  Rank: ${overall.comprank}
-  Time: ${game.time_played as int}hrs
-  Games: ${overall.games}, W: ${overall.wins}, L: ${overall.losses}, WR: ${overall.win_rate}%
-  Elims: ${game.eliminations as int}, Deaths: ${game.deaths as int}, KD: ${game.kpd}
-  Avg Elims: ${avg.eliminations_avg}, Avg Healing: ${avg.healing_done_avg}
-  Total Damage: ${game.damage_done as int}, Most Elims: ${game.eliminations_most_in_game as int}
-			
-
-${heroes}```
-			"""
-
-			chat.sendMessage(str);
-		}
-		catch (ProfileNotFound e) {
-			chat.sendMessage("Profile not found");
-		}
-
-	}
-
-	private getHeroes(time, heroes, stats) {
-		def str = "      Hero                            Time Win%\n"
-		def max = 0;
-
-		heroes.eachWithIndex { key, value, index ->
-			if(index > 4) return
-				def stat =  stats[key]
-			if(stat) {
-				max = Math.max(value as double, max as double)
-				def amn = (25 * ((value as double) / max)) as int
-
-				str += "${key.padLeft(10)} [${('-' * amn).padRight(25)}] ${value as int}h ${((stats[key].general_stats.win_percentage ?: 0) * 100) as int}%\n"
-			}
-		}
-
-		return str;
 	}
 
 
